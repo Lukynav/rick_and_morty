602f59d2c79b8cfaa75f3683f924aa9eeeade19f
Lucas Navarro <Lucasnav6@gmail.com>
2023-03-30 19:11:35 -0300

    Improve the card, (more minimalisitic)

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,27 +1,27 @@
+import { Link } from 'react-router-dom'
 import Caracteristic from './Caracteristic/Caracteristic'
 import styles from './Card.module.css'
 import TopCard from './TopCard/TopCard'
 
-const Card = (props) => {
-  const { imgUrl, name, status, species, gender, origin, setCharacters, index } = props
-
+const Card = ({ index, characterObj }) => {
+  const { id, imgUrl, name, status, species, gender, origin } = characterObj
   return (
     <article className={styles.article}>
-
-      <TopCard imgUrl={imgUrl} index={index} setCharacters={setCharacters} />
-
-      <div className={styles.allInfo}>
-        <h3 className={styles.characterName}>{name}</h3>
-        <div className={styles.characterInfo}>
-          <div className={styles.characterInfo__grid}>
+      <TopCard imgUrl={imgUrl} index={index} id={id} />
+      <Link to={`/detail/${id}`} style={{ textDecoration: 'none', color: '#000' }}>
+        <div className={styles.infoContainer}>
+          <h3 className={styles.characterName}>{name}</h3>
+          <div className={styles.caracteristics}>
             <Caracteristic iconSrc='icons/heart.svg' caract={status} />
             <Caracteristic iconSrc='icons/human.svg' caract={species} />
             <Caracteristic iconSrc='icons/gender.svg' caract={gender} />
           </div>
-          <p>First place:</p>
-          <Caracteristic caract={origin} />
+          <div className={styles.firstSeen}>
+            <p>First seen in:</p>
+            <Caracteristic caract={origin} />
+          </div>
         </div>
-      </div>
+      </Link>
     </article>
   )
 }
--- src/components/Card/TopCard/TopCard.jsx
+++ src/components/Card/TopCard/TopCard.jsx
@@ -1,8 +1,17 @@
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { deleteCharacterId, addFavorite  } from '../../../redux/actions/actions'
 import styles from './TopCard.module.css'
 
-const TopCard = ({ imgUrl, setCharacters, index }) => {
+const TopCard = ({ imgUrl, index ,id}) => {
+  const [favorite, setFavorite] = useState(false)
+  const dispatch = useDispatch()
   const onClose = () => {
-    setCharacters(state => [...state.slice(0, index), ...state.slice(index + 1)])
+    dispatch(deleteCharacterId(index))
+  }
+  
+  const addFav = () => {
+    dispatch(addFavorite(id))
   }
 
   return (
@@ -10,6 +19,22 @@ const TopCard = ({ imgUrl, setCharacters, index }) => {
       <button onClick={onClose} className={styles.buttonClose}>
         <img className='icon' src='./icons/delete.svg' alt='delete icon' />
       </button>
+      {
+        !favorite && (
+          <button onClick={addFav} className={styles.buttonFavorite}>
+            <img className='icon' src='./icons/favorite.svg' alt='delete icon' />
+          </button>
+        )
+      }
+
+      {
+        favorite && (
+          <button className={styles.buttonFavorite}>
+            <img className='icon' src='./icons/favorite_fill.svg' alt='delete icon' />
+          </button>
+        )
+      }
+      
       <img className={styles.img} src={imgUrl} alt='Rick and morty character' />
     </div>
   )
