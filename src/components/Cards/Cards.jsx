17d5190065acf4c32402e73ba2bbb93ae2e35fb1
Lucas Navarro <Lucasnav6@gmail.com>
2023-03-30 19:12:08 -0300

    refactor code

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,25 +1,17 @@
 import Card from '../Card/Card'
+import { useSelector } from 'react-redux'
+import { createCharacterObject } from './createCharacterObj.js'
 import styles from './Cards.module.css'
 
-const Cards = ({ characters, setCharacters }) => {
+const Cards = () => {
+  const characters = useSelector(state => state.characters)
   return (
     <section className={styles.cardSection}>
-      {characters && characters.map((character, index) => {
+      {characters.map((character, index) => {
         return (
-          <Card
-            key={index}
-            index={index}
-            status={character.status}
-            imgUrl={character.image}
-            name={character.name}
-            gender={character.gender}
-            origin={character.origin.name}
-            species={character.species}
-            setCharacters={setCharacters}
-          />
+          <Card key={index} index={index} characterObj={createCharacterObject(character)} />
         )
       })}
-
     </section>
   )
 }
