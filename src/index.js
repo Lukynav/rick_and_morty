ac7ceb9d1e74a05214fceb5f431227b7896a3ff5
Lucas Navarro <Lucasnav6@gmail.com>
2023-03-30 19:10:39 -0300

    Fix bugs index, and add redux

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import App from './App'
+import Router from './Router'
 import './global.css'
+import { Provider } from 'react-redux'
+import store from './redux/store'
 
 // Where I render my content
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 // What I render
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
+  </Provider>
 )
