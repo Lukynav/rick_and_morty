249cfe7ff422ff764c3e3a205be5cf45f3219112
Lucas Navarro <Lucasnav6@gmail.com>
2023-03-30 21:51:25 -0300

    add favorite route

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,3 +1,4 @@
+import { useSelector } from 'react-redux'
 import { Routes, Route } from 'react-router-dom'
 import Cards from './components/Cards/Cards'
 import Navegation from './components/Navegation/Navegation'
@@ -6,12 +7,16 @@ import Detail from './routes/detail/detail'
 import Login from './routes/login/login'
 
 const App = () => {
+  const characters = useSelector(state => state.characters)
+  const favorites = useSelector(state => state.favorites)
+  
   return (
     <main>
       <Navegation />
       <Routes>
         <Route path='/' element={<Login />} />
-        <Route path='/home' element={<Cards />} />
+        <Route path='/home' element={<Cards characters={characters} />} />
+        <Route path='/favorites' element={<Cards characters={favorites}/>} />
         <Route path='/about' element={<About />} />
         <Route path='/detail/:id' element={<Detail />} />
       </Routes>
