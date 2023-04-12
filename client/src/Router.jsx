3e40c206a2493f0069b22a99e9b55dbf951fff41
Lucas Navarro <Lucasnav6@gmail.com>
2023-04-12 13:39:22 -0300

    REFACTOR: change the name of the variables

--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -4,16 +4,16 @@ import { Cards, Navegation } from './components'
 import { About, Detail, Favorites, Login } from './routes'
 
 const App = () => {
-  const characters = useSelector(state => state.characters)
-  const favoritesArr = useSelector(state => state.favorites)
+  const CHARACTES = useSelector(state => state.characters)
+  const FAVORITES = useSelector(state => state.favorites)
 
   return (
     <main>
       <Navegation />
       <Routes>
         <Route path='/' element={<Login />} />
-        <Route path='/home' element={<Cards characters={characters} />} />
-        <Route path='/favorites' element={<Favorites characters={favoritesArr} />} />
+        <Route path='/home' element={<Cards characters={CHARACTES} />} />
+        <Route path='/favorites' element={<Favorites characters={FAVORITES} />} />
         <Route path='/about' element={<About />} />
         <Route path='/detail/:id' element={<Detail />} />
       </Routes>
